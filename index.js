b86466bd83f075906f52efb05aedeee9acb538f0
neoteristis <anjou.raphael@gmail.com>
2022-05-15 10:27:03 +0200

    feat: start tutorial 2
    Goal: building an api registering, showing and modifying books in a list

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('My first REST API'));
+// Where we will keep books
+let books = [];
 
-app.listen(port, () => {
-    console.log('Listening on port ' + port);
+// --- Configure app ---
+// Use CORS middleware
+app.use(cors());
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Beginning of the program
+app.post('/post-book', (req, res) => {
+    const book = req.body;
+
+    // Output the book to the console for debugging
+    console.log(book);
+    books.push(book);
+
+    res.send('Book is added to the database');
 });
 
+// New book page
+app.get('/new-book', (req, res) => {
+    res.sendFile(__dirname + "/new-book.html");
+})
+
+// Home page
+app.get('/', (req, res) => {
+    res.send("Home page of our API");
+})
+
+// Show a list of all the stored books
+app.get('/book-list', (req, res) => {
+    res.json(books);
+});
+
+// Listen for any incoming request
+app.listen(port, () => console.log(`Listening on port ${port}`));
